1fd290f058fc4f5b1ac80af7a4e2f5e7ebf2a729
selek4 <65973396+selek4@users.noreply.github.com>
2021-04-06 15:09:28 -0400

    Add files via upload

---
+++ FirstRecurring.ps1
@@ -0,0 +1,21 @@
+﻿Clear-Host
+
+function get-firstrecurring {
+    [CmdletBinding()]
+    param (
+    [string]$foo
+    )
+    $InputString = @{}
+    $CharArray = $foo.ToCharArray()
+
+    foreach ($char in $CharArray) {
+        if($char -in $InputString.Keys) {
+            $char
+            break
+        }
+        else {
+            $InputString[$char] = $null
+        }
+    }
+}
+get-firstrecurring abcdedcba
